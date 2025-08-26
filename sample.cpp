15c5cb5a13c4708012eeee7eb650285b3527f6c1
HelplessMelon <152384309+HelplessMelon@users.noreply.github.com>
2025-08-26 00:32:47 +0000

    done

--- sample.cpp
+++ sample.cpp
@@ -1,7 +1,17 @@
-// YOUR NAME HERE
+// KEVIN JANENKO
 #include <iostream>
 using namespace std;
 
 int main () {
+    int int1,int2;
+
+    cout << "Enter first integer: ";
+    cin >> int1;
+    cout << "Enter second integer: ";
+    cin >> int2;
+
+    cout << "The sum is "<<int1 + int2<< ".\n";
+
+    return 0;
 
 }
